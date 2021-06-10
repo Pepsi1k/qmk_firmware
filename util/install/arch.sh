56f068ea25f844489fb4572eda418fe3f0f9c455
Pepsi1k <megasharff@gmail.com>
2021-06-10 03:46:43 +0300

    add keymap piano, downgrade arg-gcc-8.1

--- util/install/arch.sh
+++ util/install/arch.sh
@@ -7,7 +7,7 @@ _qmk_install() {
         base-devel clang diffutils gcc git unzip wget zip python-pip \
         avr-binutils arm-none-eabi-binutils arm-none-eabi-gcc \
         arm-none-eabi-newlib avrdude dfu-programmer dfu-util
-    sudo pacman --needed --noconfirm -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.3.0-1-x86_64.pkg.tar.xz
+    sudo pacman --needed --noconfirm -U https://archive.archlinux.org/packages/a/avr-gcc/avr-gcc-8.1.0-1-x86_64.pkg.tar.xz
     sudo pacman --needed --noconfirm -S avr-libc # Must be installed after the above, or it will bring in the latest avr-gcc instead
 
     sudo pacman --needed  --noconfirm -S hidapi  # This will fail if the community repo isn't enabled
